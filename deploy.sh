94c58dc0a91d134248ce8b0232bf873509336b23
Junha Park <jpark3@scu.edu>
2020-08-14 20:29:55 -0700

    Added aborts to deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,19 @@ SERVERLESS_CONFIG=$BASE_DIR/serverless.yml
 
 cd $SRC_DIR
 echo "Building app to $BUILD_DIR..."
-env GOOS=linux GOARCH=amd64 go build -o $BUILD_DIR/spekkio
+env GOOS=linux GOARCH=amd64 go build -o $BUILD_DIR/spekkio 2> /dev/null
+rc=$?
+if [[ $rc -ne 0 ]]; then
+    echo "Build failed with exit status $rc! Aborting deploy."
+    exit 1
+fi
 echo "Deploying via serverless framework..."
-serverless deploy
-echo "Done!"
+cd $BASE_DIR
+serverless deploy 2> /dev/null
+rc=$?
+if [[ $rc -eq 0 ]]; then
+    echo "Done!"
+else
+    echo "Deploy failed with exit status $rc!"
+    exit 1
+fi
