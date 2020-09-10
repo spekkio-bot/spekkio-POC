4eee1ed16e1949a13fadcb58e8c736e383f54cfa
Junha Park <48537888+park-junha@users.noreply.github.com>
2020-09-09 19:36:11 -0700

    (0.2.11) Tests for gqlbuilder mutation Build() (#51)

--- src/queries/graphql/gqlbuilder_test.go
+++ src/queries/graphql/gqlbuilder_test.go
@@ -1,6 +1,7 @@
 package gqlbuilder
 
 import (
+	"io/ioutil"
 	"testing"
 )
 
@@ -54,12 +55,48 @@ func TestMutationBuilder(t *testing.T) {
 		Return: []string{"testReturnSingleValue"},
 	})
 
-	got, err := testInput.BuildQuery()
-	want := "mutation TestMutation {testAlias: testMutation(input: {testFieldOne: \"1080\", testFieldTwo: \"2080\"}) { testReturnId testReturnValue } testAnotherAlias: testMutation(input: {testFieldOne: \"3080\", testFieldTwo: \"3090\"}) { testReturnAnotherId testReturnAnotherValue } testNoAliasMutation(input: {singleTestInput: \"980\"}) { testReturnSingleValue } }"
+	gotEncoded, err := testInput.Build()
 
 	if err != nil {
-		t.Errorf("Failed with error %s", err.Error())
-	} else if got != want {
-		t.Errorf("BuildQuery() returned:\n\"%s\"\n\nExpected:\n\"%s\"\n", got, want)
+		t.Errorf("(Mutation) Build() failed with error %s\n", err.Error())
+	}
+
+	got, _ := ioutil.ReadAll(gotEncoded)
+	want := "{\"query\":\"mutation TestMutation {testAlias: testMutation(input: {testFieldOne: \\\"1080\\\", testFieldTwo: \\\"2080\\\"}) { testReturnId testReturnValue } testAnotherAlias: testMutation(input: {testFieldOne: \\\"3080\\\", testFieldTwo: \\\"3090\\\"}) { testReturnAnotherId testReturnAnotherValue } testNoAliasMutation(input: {singleTestInput: \\\"980\\\"}) { testReturnSingleValue } }\"}"
+
+	if string(got) != want {
+		t.Errorf("(Mutation) Build() returned:\n\"%s\"\n\nExpected:\n\"%s\"\n", got, want)
+	}
+}
+
+func TestNamelessMutationBuilder(t *testing.T) {
+	testInput := &Mutations{
+		Name:      "TestMutation",
+		Mutations: []Mutation{},
+	}
+
+	testInput.Mutations = append(testInput.Mutations, Mutation{
+		Name:  "",
+		Alias: "shouldFail",
+		Inputs: []MutationInput{
+			{
+				Key:   "thisShouldFail",
+				Value: "666",
+			},
+		},
+		Return: []string{"thisShouldNotReturn"},
+	})
+
+	_, err := testInput.Build()
+	want := "no mutation name specified"
+
+	if err == nil {
+		t.Errorf("(Mutation) Build() should have failed with error %s but did not fail\n", want)
+	}
+
+	got := err.Error()
+
+	if got != want {
+		t.Errorf("(Mutation) Build() failed with unexpected error\n\ngot %s\nwant %s\n", got, want)
 	}
 }
